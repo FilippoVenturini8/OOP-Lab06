9d6d823b7c5b94b5d031dab812e10fb7d6cf858a
Filippo Venturini <filippo.venturini8@studio.unibo.it>
2020-11-04 19:14:28 +0100

    Final fix RobotTest

--- src/it/unibo/oop/lab/exception1/BaseRobotTest.java
+++ src/it/unibo/oop/lab/exception1/BaseRobotTest.java
@@ -88,21 +88,25 @@ public final class BaseRobotTest {
          * Repeatedly move the robot up and down until the battery is completely
          * exhausted.
          */
-        while (r2.getBatteryLevel() > 0) {
+        try {
+        	while (r2.getBatteryLevel() > 0) {
+                r2.moveUp();
+                r2.moveDown();
+            }
+        	 // verify battery level:
+            // expected, actual, delta (accepted error as we deal with decimal
+            // values: in this case we accept NO ERROR, which is generally bad)
+            assertEquals(0d, r2.getBatteryLevel(), 0);
+            // verify position: same as start position
+            assertEquals("[CHECKING ROBOT INIT POS Y]", 0, r2.getEnvironment().getCurrPosY());
+            // out of world: returns false
             r2.moveUp();
-            r2.moveDown();
+            // recharge battery
+            r2.recharge();
+            // verify battery level
+            assertEquals(100, r2.getBatteryLevel(), 0);
+        }catch(NotEnoughBatteryException e) {
+        	Assert.assertNotNull(e.getMessage());
         }
-        // verify battery level:
-        // expected, actual, delta (accepted error as we deal with decimal
-        // values: in this case we accept NO ERROR, which is generally bad)
-        assertEquals(0d, r2.getBatteryLevel(), 0);
-        // verify position: same as start position
-        assertEquals("[CHECKING ROBOT INIT POS Y]", 0, r2.getEnvironment().getCurrPosY());
-        // out of world: returns false
-        r2.moveUp();
-        // recharge battery
-        r2.recharge();
-        // verify battery level
-        assertEquals(100, r2.getBatteryLevel(), 0);
     }
 }
